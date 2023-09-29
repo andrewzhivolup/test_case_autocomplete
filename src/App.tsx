ce7b824a8cafb84daa087f2f8bdd4dc2216009dc
Andrew Zhivolup <andrewzhivolup@yandex.ru>
2023-09-29 16:38:52 +0300

    add eslint, fix all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.scss';
 
 function App() {
     return (
-        <div></div>
+        <div>
+
+        </div>
     );
 }
 
--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,4 +1,4 @@
-import countries from "./countries.json";
+import countries from './countries.json';
 
 export interface CountryInfo {
   name: string;
@@ -7,23 +7,23 @@ export interface CountryInfo {
 }
 
 export function getCountryByName(countryName: string): Promise<CountryInfo[]> {
-  return new Promise((resolve) => {
-    setTimeout(resolve, getRandom(100, 800));
-  }).then(() => {
-    if (typeof countryName !== "string" || !countryName) {
-      return [];
-    }
+    return new Promise((resolve) => {
+        setTimeout(resolve, getRandom(100, 800));
+    }).then(() => {
+        if (typeof countryName !== 'string' || !countryName) {
+            return [];
+        }
 
-    const searchText = countryName.toLocaleLowerCase();
+        const searchText = countryName.toLocaleLowerCase();
 
-    return countries.filter(
-      (x) =>
-        x.name.toLocaleLowerCase().startsWith(searchText) ||
+        return countries.filter(
+            (x) =>
+                x.name.toLocaleLowerCase().startsWith(searchText) ||
         x.fullName.toLocaleLowerCase().startsWith(searchText)
-    );
-  });
+        );
+    });
 }
 
 function getRandom(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
